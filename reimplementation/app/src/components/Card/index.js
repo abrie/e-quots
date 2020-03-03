f380292415627f80f6af03c13900084ac48b49a9
abrie <abrie@users.noreply.github.com>
2020-03-03 15:07:18 -0500

    Add rudimentary layout to cards.

--- reimplementation/app/src/components/Card/index.js
+++ reimplementation/app/src/components/Card/index.js
@@ -31,6 +31,46 @@ export default function(params) {
     });
   };
 
+  const Sections = ({ sections }) => {
+    return sections.map((section, key) => {
+      return <Section section={section} key={key} />;
+    });
+  };
+
+  const Section = ({ section }) => {
+    return (
+      <div className="section">
+        <div className="sectionHeader">{section.header}</div>
+        <div className="sectionQuestions">
+          <Questions section={section} />
+        </div>
+      </div>
+    );
+  };
+
+  const Questions = ({ section }) => {
+    return section.questions.map((question, key) => {
+      return (
+        <Question
+          question={question}
+          choiceCount={section.choiceSet.length}
+          key={key}
+        />
+      );
+    });
+  };
+
+  const Question = ({ question, choiceCount }) => {
+    return (
+      <div className="question">
+        <div className="questionText"> {question.text}</div>
+        <div className="questionChoices" data-count={choiceCount}>
+          <Choices question={question} />
+        </div>
+      </div>
+    );
+  };
+
   const Choices = ({ question }) => {
     return question.choices.map((choice, key) => {
       const id = `${question.id}-${choice}`;
@@ -50,47 +90,28 @@ export default function(params) {
     });
   };
 
-  const Questions = ({ section }) => {
-    return section.questions.map((question, key) => {
-      return <Question question={question} key={key} />;
-    });
+  const Instructions = ({ text }) => {
+    return <div className="instructions">{text}</div>;
   };
 
-  const Question = ({ question }) => {
+  const Reference = ({ href }) => {
     return (
-      <div className="question">
-        <div className="questionText"> {question.text}</div>
-        <div className="questionChoices">
-          <Choices question={question} />
-        </div>
+      <div className="reference">
+        <a href={template.pdf}>{template.pdf}</a>
       </div>
     );
-  };
-
-  const Section = ({ section }) => {
-    return (
-      <div className="section">
-        <div className="sectionHeader">{section.header}</div>
-        <div className="sectionQuestions">
-          <Questions section={section} />
-        </div>
-      </div>
-    );
-  };
-
-  const renderSections = sections => {
-    return sections.map((section, key) => {
-      return <Section section={section} key={key} />;
-    });
   };
 
   if (template) {
     return (
       <>
-        <div className="name">{template.name}</div>
-        <button onClick={handleExport}>upload</button>
-        <div className="instructions">{template.instructions}</div>
-        {renderSections(template.sections)}
+        <div className="name">{template.title}</div>
+        <Reference href={template.pdf} />
+        <Instructions text={template.instructions} />
+        <Sections sections={template.sections} />
+        <div className="controls">
+          <button onClick={handleExport}>upload</button>
+        </div>
       </>
     );
   } else {
