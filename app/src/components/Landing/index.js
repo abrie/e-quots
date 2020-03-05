fdc4567761e1d1247864db83dc17e9a52f71c7b3
abrie <abrie@users.noreply.github.com>
2020-03-05 11:59:09 -0500

    More words.

--- app/src/components/Landing/index.js
+++ app/src/components/Landing/index.js
@@ -6,19 +6,19 @@ export default function() {
     <div className="landing">
       <h1 className="title">Infection Control</h1>
       <p>
-        This app is an implementation of the CDC's{" "}
+        Welcome to this implementation of the CDC's{" "}
         <a href="https://www.cdc.gov/infectioncontrol/tools/quots.html">
           Quick Check Tools (QUOTS)
         </a>
         .
       </p>
       <p>
-        Use this tool to encourage and measure best practices in health care
-        settings.
+        Use this tool to measure and encourage best practices in clinical
+        settings. It will help limit the spread of infectious agents.
       </p>
       <p>To get started, select a card from the header menu above.</p>
       <p>
-        For technical information, see the
+        For technical information, see the&nbsp;
         <a href="https://github.com/abrie/e-quots">source code on Github.</a>
       </p>
     </div>
